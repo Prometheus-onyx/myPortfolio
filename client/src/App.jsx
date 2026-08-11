43ec5dd5ddf67a3064efd244dd846aac3c750e7a
O~P~L <stevebickookumu@gmail.com>
2026-08-11 10:55:13 +0300

    Resolved Analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import AppRoutes from './routes/AppRoutes';
 import './App.css';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
